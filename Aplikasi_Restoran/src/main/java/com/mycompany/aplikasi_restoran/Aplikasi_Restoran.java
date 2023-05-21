45b261bdfdd2395ffb373c5baedd7f5320a2850f
agoyy17 <134230314+agoyy17@users.noreply.github.com>
2023-05-22 02:14:45 +0700

    Add files via upload

---
+++ Aplikasi_Restoran/src/main/java/com/mycompany/aplikasi_restoran/Aplikasi_Restoran.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplikasi_restoran;
+
+/**
+ *
+ * @author User
+ */
+public class Aplikasi_Restoran {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
