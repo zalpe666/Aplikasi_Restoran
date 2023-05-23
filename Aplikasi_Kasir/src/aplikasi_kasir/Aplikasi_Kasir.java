8e74705f8b773afe55a14fc51949a9194b0d27ae
agoyy17 <134230314+agoyy17@users.noreply.github.com>
2023-05-23 15:32:39 +0700

    Add files via upload

---
+++ Aplikasi_Kasir/src/aplikasi_kasir/Aplikasi_Kasir.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasi_kasir;
+
+/**
+ *
+ * @author User
+ */
+public class Aplikasi_Kasir {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Log_in l = new Log_in();
+        l.setVisible(true);
+    }
+    
+}
---
+++ Aplikasi_Kasir/src/aplikasi_kasir/koneksi.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplikasi_kasir;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author User
+ */
+public class koneksi {
+    private String url="jdbc:mysql://localhost/db_restoran";
+    private String username_xampp = "root";
+    private String password_xampp = "";
+    private Connection con;
+    
+    public void connect(){
+        try {
+            con = DriverManager.getConnection(url,username_xampp, password_xampp);
+            System.out.println("Koneksi Berhasil");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+    
+}
